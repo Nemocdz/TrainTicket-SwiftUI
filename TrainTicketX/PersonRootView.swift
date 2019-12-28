cf62fe01567a92407f2f379e9ca79ccbc1ddd84b
Nemocdz <nemocdz@gmail.com>
2019-12-28 17:57:18 +0800

    refactor: var

--- TrainTicketX/PersonRootView.swift
+++ TrainTicketX/PersonRootView.swift
@@ -20,18 +20,18 @@ struct PersonRootView: View {
                 }
                 Section(header: Text("数据")) {
                     DataInfoRow(title: "总金额", detail: "\(String(format: "%.1f", dataCenter.tickets.reduce(0, { $0 + $1.money }))) 元")
-                    DataInfoRow(title: "最常用路线", detail: "\(rareLine())")
+                    DataInfoRow(title: "最常用路线", detail: "\(rareLine)")
                     DataInfoRow(title: "总公里数", detail: "\(Int(dataCenter.tickets.reduce(0, { $0 + $1.distance }))) 公里")
-                    DataInfoRow(title: "总时长", detail: "\(totalMinute() / (60 * 60)) 天 \(totalMinute() % (60 * 60) / 60) 小时")
+                    DataInfoRow(title: "总时长", detail: "\(totalMinute / (60 * 60)) 天 \(totalMinute % (60 * 60) / 60) 小时")
                     DataInfoRow(title: "总行程数", detail: "\(dataCenter.tickets.count) 趟")
-                    DataInfoRow(title: "去过的城市数", detail: "\(cityCount()) 个")
+                    DataInfoRow(title: "去过的城市数", detail: "\(cityCount) 个")
                 }
             }
             .navigationBarTitle("汇总")
         }
     }
     
-    func cityCount() -> Int {
+    var cityCount:Int {
         var citys = Set<String>()
         dataCenter.tickets.forEach {
             citys.insert(TicketHelper.city(from: $0.startStation))
@@ -40,7 +40,7 @@ struct PersonRootView: View {
         return citys.count
     }
     
-    func rareLine() -> String {
+    var rareLine:String {
         var map = [String: Int]()
         dataCenter.tickets.forEach {
             let line = "\(TicketHelper.city(from: $0.startStation))-\(TicketHelper.city(from: $0.endStation))"
@@ -54,7 +54,7 @@ struct PersonRootView: View {
         return map.max(by: { $0.value > $1.value })?.key ?? ""
     }
     
-    func totalMinute() -> Int {
+    var totalMinute:Int {
         dataCenter.tickets.reduce(0) { $0 + $1.runTime }
     }
 }
